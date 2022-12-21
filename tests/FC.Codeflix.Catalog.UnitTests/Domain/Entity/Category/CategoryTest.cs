0cc30f1573edfa19dba29c199c63107faecc84ae
Jhonattan Oliveira <jhonattanoliveira@hotmail.com>
2022-12-21 00:12:15 -0300

    Adicionar arquivos de projeto.

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,109 @@
+﻿using FC.Codeflix.Catalog.Domain.Exceptions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NuGet.Frameworks;
+using System.Diagnostics.SymbolStore;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Category;
+
+public class CategoryTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain","Category - Aggregates")]
+    public void Instantiate()
+    {
+        //Arrange
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description"
+        };
+        var dateTimeBefore = DateTime.Now;
+
+        //Act
+        var category = new DomainEntity.Category(validData.Name, validData.Description);
+        var dateTimeAfter = DateTime.Now;
+
+        //Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt > dateTimeBefore);
+        Assert.True(category.CreatedAt < dateTimeAfter);
+        Assert.True(category.IsActive);
+    }
+
+    [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        //Arrange
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description"
+        };
+        var dateTimeBefore = DateTime.Now;
+
+        //Act
+        var category = new DomainEntity.Category(validData.Name, validData.Description, isActive);
+        var dateTimeAfter = DateTime.Now;
+
+        //Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt > dateTimeBefore);
+        Assert.True(category.CreatedAt < dateTimeAfter);
+        Assert.Equal(isActive, category.IsActive);
+    }
+
+    //       
+    //Nome não pode ser empty 
+    //
+    [Theory(DisplayName = nameof(InstantiateErrorWhenNameIsEmpty))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("   ")]
+    public void InstantiateErrorWhenNameIsEmpty(string? name)
+    {
+        Action action = 
+            () => new DomainEntity.Category(name!, "Category Description");
+
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Name should not be empty or null", exception.Message);
+    }
+
+    //
+    // Description não pode ser null
+    //
+    [Fact(DisplayName = nameof(InstantiateErrorWhenDescriptionIsNull))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void InstantiateErrorWhenDescriptionIsNull()
+    {
+        Action action =
+            () => new DomainEntity.Category("Category Name",null!);
+
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Description should not be empty or null", exception.Message);
+    }
+
+    //
+    // Nome dev eter no minimo 3 caracteres
+    //
+
+    //
+    // Nome deve ter no máximo 255 caracteres
+    //
+
+    //
+    // Descrição deve ter no máximo 10_000 caracteres
+    //
+}
