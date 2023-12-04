bb0ceb3eddf2ca68c9f190c1e86039f6262f8d68
jhonattan oliveira <jhonattan.oliveira@gestran.com.br>
2023-12-04 13:17:35 -0300

    parcial, aula 4.2 Teste do UseCase CrateCategory

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,17 @@
+﻿using Moq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
+
+public class CreateCategoryTest
+{
+    [Fact(DisplayName = nameof(CreateCategory))]
+    [Trait("Application","CreateCategory - Use Cases")]
+    public void CreateCategory()
+    {
+        var respositoryMock = new Mock<ICategoryRepository>();
+        var unitOfWorkMock = new Mock<IunitOfWork>();
+        var useCase = new CreateCategory(
+            respositoryMock.Object,
+            unitOfWorkMock.Object);
+    }
+}
